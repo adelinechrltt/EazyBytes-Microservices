8e7db18cca126cebaa25b1ab40e5572e7719b9c9
Adeline Charlotte <adeline.chrltt@gmail.com>
2025-08-01 01:01:50 +0700

    Create Exception & ExceptionHandler classes

---
+++ cards/src/main/java/com/adeline/cards/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,11 @@
+package com.adeline.cards.exceptions;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+//    public ResponseEntity<ErrorResponseDto> handleGlobalException(){
+//
+//    }
+}

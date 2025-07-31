8acdb835f2e2a87b92adefa9b601b4e7c11cc9cf
Adeline Charlotte <adeline.chrltt@gmail.com>
2025-07-31 21:29:13 +0700

    Enhance documentation with @OpenAPIDefinition annotation

--- accounts/src/main/java/com/adeline/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/adeline/accounts/AccountsApplication.java
@@ -1,11 +1,33 @@
 package com.adeline.accounts;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Accounts Microservice REST API Documentation",
+                description = "EazyBank Accounts Microservice REST API Documentation",
+                version = "v1",
+                contact = @Contact(
+                        name = "Adeline"
+                ),
+                license = @License(
+                        name = "Apache 2.0"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "EazyBank Accounts Microservices REST API Documentation",
+                url = "https://www.eazybytes.com/swagger0ui.html"
+        )
+)
 public class AccountsApplication {
 
     public static void main(String[] args) {

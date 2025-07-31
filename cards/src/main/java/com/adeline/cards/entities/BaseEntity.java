b04115df9491ffdeb9f095d245d3c8d48e32bbdb
Adeline Charlotte <adeline.chrltt@gmail.com>
2025-08-01 01:22:31 +0700

    Add auditing annotations to cards microservice root entry point & base entity class

--- cards/src/main/java/com/adeline/cards/entities/BaseEntity.java
+++ cards/src/main/java/com/adeline/cards/entities/BaseEntity.java
@@ -1,6 +1,7 @@
 package com.adeline.cards.entities;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Data;
 import lombok.ToString;
@@ -8,10 +9,12 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 @Data
 @ToString
 public class BaseEntity {

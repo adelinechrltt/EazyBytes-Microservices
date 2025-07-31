61d77361093f2c9b7b4d156e1baf06d29b26e439
Adeline Charlotte <adeline.chrltt@gmail.com>
2025-08-01 00:50:57 +0700

    Set card entity ID generation logic

--- cards/src/main/java/com/adeline/cards/entities/Card.java
+++ cards/src/main/java/com/adeline/cards/entities/Card.java
@@ -1,8 +1,6 @@
 package com.adeline.cards.entities;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,6 +13,7 @@ public class Card extends BaseEntity {
 
     @Column(name="card_id")
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long cardId;
 
     @Column(name="mobile_number")

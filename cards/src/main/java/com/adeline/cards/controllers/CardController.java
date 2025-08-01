abfb8a2eb7126ef0febf94d6b3d1ab9b09552528
Adeline Charlotte <adeline.chrltt@gmail.com>
2025-08-01 14:51:50 +0700

    Create CRUD CardController endpoints

--- cards/src/main/java/com/adeline/cards/controllers/CardController.java
+++ cards/src/main/java/com/adeline/cards/controllers/CardController.java
@@ -1,15 +1,19 @@
 package com.adeline.cards.controllers;
 
+import com.adeline.cards.constants.CardConstants;
 import com.adeline.cards.dtos.CardDto;
+import com.adeline.cards.dtos.ResponseDto;
+import com.adeline.cards.services.ICardsService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @Tag(
@@ -22,10 +26,59 @@ import org.springframework.web.bind.annotation.RestController;
 @Validated
 public class CardController {
 
+    private ICardsService cardsService;
+
     @GetMapping("/fetch")
     public ResponseEntity<CardDto> fetchCard(
             @RequestParam String mobileNumber
     ) {
+        CardDto cardDto = cardsService.fetchCard(mobileNumber);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(cardDto);
+    }
 
+    @PostMapping("/create")
+    public ResponseEntity<ResponseDto> createCard(
+            @RequestBody @Valid CardDto cardDto
+    ) {
+        cardsService.createCard(cardDto);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new ResponseDto(CardConstants.STATUS_201, CardConstants.MESSAGE_201));
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<ResponseDto> updateCard(
+            @RequestBody @Valid CardDto cardDto
+    ){
+        boolean isUpdated = cardsService.updateCard(cardDto);
+        if(isUpdated){
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(new ResponseDto(CardConstants.STATUS_200, CardConstants.MESSAGE_200));
+        } else {
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ResponseDto(CardConstants.STATUS_500, CardConstants.MESSAGE_500));
+        }
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<ResponseDto> deleteCard(
+            @RequestParam
+            @Pattern(regexp="(^$|[0-9]{10})", message="Mobile number must be 10 digits")
+            String mobileNumber
+            ) {
+        boolean isDeleted = cardsService.deleteCard(mobileNumber);
+        if(isDeleted){
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(new ResponseDto(CardConstants.STATUS_200, CardConstants.MESSAGE_200));
+        } else {
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ResponseDto(CardConstants.STATUS_500, CardConstants.STATUS_500));
+        }
     }
 }

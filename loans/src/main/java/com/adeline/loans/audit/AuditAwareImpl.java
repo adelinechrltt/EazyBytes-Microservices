a001033fb2ead68a8d96404ad2610de10ee7b0cd
Adeline Charlotte <adeline.chrltt@gmail.com>
2025-08-01 16:26:33 +0700

    Create auditor implementation

---
+++ loans/src/main/java/com/adeline/loans/audit/AuditAwareImpl.java
@@ -0,0 +1,15 @@
+package com.adeline.loans.audit;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component("auditAwareImpl")
+public class AuditAwareImpl implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of("ACCOUNTS_MS"); /// --> hard-coding values until we reach the security section
+    }
+}
